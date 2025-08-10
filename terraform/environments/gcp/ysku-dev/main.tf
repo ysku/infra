f33b38826b5d365fc4930e4d706268ce4fafe0aa
ysku <yusuke1581@gmail.com>
2025-08-10 17:50:57 +0900

    add artifact registry for voice-assistant

--- terraform/environments/gcp/ysku-dev/main.tf
+++ terraform/environments/gcp/ysku-dev/main.tf
@@ -3,3 +3,10 @@ module "gh-actions-oidc" {
   project      = var.project_id
   repositories = var.repositories
 }
+
+resource "google_artifact_registry_repository" "voice-assistant" {
+  project       = var.project_id
+  location      = "asia-northeast1"
+  repository_id = "voice-assistant"
+  format        = "DOCKER"
+}
